0258817ad4d7eca1223480aae3f00d7b2fc6ff05
andrebrener <brener.andre@gmail.com>
2018-10-25 16:10:41 -0300

    Typo in table name

--- get_data/sql/games_info.sql
+++ get_data/sql/games_info.sql
@@ -6,7 +6,7 @@ CREATE TABLE games_info (
  round_id integer NOT NULL REFERENCES rounds(id),
  home_team_id integer REFERENCES teams_info(id),
  away_team_id integer REFERENCES teams_info(id),
- status_id integer NOT NULL REFERENCES game_status(id),
+ status_id integer NOT NULL REFERENCES games_status(id),
  home_team_score integer NOT NULL,
  away_team_score integer NOT NULL,
  stadium_id integer NOT NULL,
